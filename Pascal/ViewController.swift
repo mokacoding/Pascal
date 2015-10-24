248a456356971537af066052ef6fa4726ea70879
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-10-24 17:19:11 +1100

    Add some views showcasing how to set colors and gradients

--- Pascal/ViewController.swift
+++ Pascal/ViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+  @IBOutlet weak var firstView: UIView!
+  @IBOutlet weak var secondView: UIView!
+  @IBOutlet weak var thirdView: UIView!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // How to pick a color
+    firstView.backgroundColor = UIColor.flatNavyBlueColor()
+
+    // How to make a color darke
+    secondView.backgroundColor = UIColor.flatNavyBlueColor().darkenByPercentage(0.1)
+
+    // How to make a gradient
+    thirdView.backgroundColor = UIColor.init(gradientStyle: UIGradientStyle.LeftToRight,
+      withFrame: thirdView.frame,
+      andColors: [UIColor.flatYellowColor(), UIColor.flatPurpleColor()])
+  }
 
 
 }
